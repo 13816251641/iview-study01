7f436d73d7e5824e5d87ef4e767c103fdd9abbe3
181268 <lujieni1017>
2020-06-18 14:55:01 +0800

    代码提交

--- config/http.js
+++ config/http.js
@@ -1,14 +1,64 @@
 import axios from 'axios'
+import Cookies from 'js-cookie';
 
 
 let service = axios.create({
     baseURL: 'http://localhost:8080/vue/',
+    withCredentials: true,//允许携带cookie,不加无法传输cookie
     timeout: 5000,
     headers: {
         'content-type': 'application/json',
         'token':'14a1347f412b319b0fef270489f'
     }
 });
+
+
+// request拦截器
+service.interceptors.request.use(config => {
+    let headers = {};
+    let token = Cookies.get('inner-token');
+    if (token){
+        headers['token'] = token;
+    }
+    config.headers = headers;//这里会覆盖service中的headers
+    return config;
+}, error => {
+    // Do something with request error
+    console.log(error);// for debug
+    Promise.reject(error);
+});
+
+
+// respone拦截器
+service.interceptors.response.use(
+    response => {
+        if (process.env.ENV_CONFIG !== 'prod') {
+            console.log(response);
+        }
+        //统一异常处理
+        if (response && response.data) {
+            const data = response.data;
+            //未登录则重定向
+            if (data.errorCode === '302'){ //清除session,返回到百度
+                window.location.href = 'http://www.baidu.com';
+            }
+            return Promise.resolve(response.data);//程序ok,将结果返回给前端
+        }
+        return Promise.reject();
+
+    },
+    error => {
+        console.log('err' + error);// for debug
+        Message({
+            message: error.message,
+            type: 'error',
+            duration: 3 * 1000,
+            showClose: true
+        });
+        return Promise.reject(error);
+    }
+);
+
 
 export default {
     service,
--- src/views/Message/Message01.vue
+++ src/views/Message/Message01.vue
@@ -1,21 +1,16 @@
 <template>
-    <Button @click="time">Displays a 10 second prompt</Button>
+    <div>
+        <Button @click="time">Displays a 10 second prompt</Button>
+        <Button @click="useGet">get</Button>
+        <Button @click="usePost">post</Button>
+    </div>
 </template>
 
 <script>
     export default {
         methods:{
-            time(){
-                console.log(process.env);
-                console.log(process.env123);
-                this.$Message.info({
-                    content: 'I will be gone in 10 seconds',
-                    duration: 10,
-                    closable: true
-                });
-
-
-            /*    this.http.get('/hello',{
+            useGet(){
+                this.http.get('/hello',{
                     'name':'wahaha',
                     'age': 28
                 }).then(res=>{
@@ -24,8 +19,10 @@
                 }).catch(function (res){ //服务端超时的话会进入catch
                     console.log('catch');
                     console.log(res);
-                });*/
-
+                });
+            },
+            usePost(){
+                /* 异步请求 */
                 this.http.post('/bye',{
                     'name':'wahaha',
                     'age': 28
@@ -36,6 +33,17 @@
                     console.log('catch-post');
                     console.log(res);
                 });
+            },
+            time(){
+                console.log(process.env);
+                console.log(process.env123);
+                this.$Message.info({
+                    content: 'I will be gone in 10 seconds',
+                    duration: 10,
+                    closable: true
+                });
+
+
             }
         }
     }
